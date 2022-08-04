f0207807985ab7c1516d52aeeb8584d7a22508b8
KiranitSS <vlastelingt1@gmail.com>
2022-08-04 14:51:11 +0200

    Tribe view issue fixed

--- UnrealTribe_Wpf/obj/Release/net6.0-windows/UnrealTribe_Wpf.AssemblyInfo.cs
+++ UnrealTribe_Wpf/obj/Release/net6.0-windows/UnrealTribe_Wpf.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnrealTribe_Wpf")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnrealTribe_Wpf")]
